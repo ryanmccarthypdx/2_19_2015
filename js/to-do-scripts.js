0db0ce9538a16755594e6fb2323d63d0444bd107
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-19 11:54:33 -0800

    Kill the to-do project

--- js/to-do-scripts.js
+++ js/to-do-scripts.js
@@ -1,21 +1,26 @@
+// var Task = {
+//   done: false,
+//
+// }
+
 $(document).ready(function () {
   var allTasks = [];
   $("form#new-task").submit(function(event) {
     event.preventDefault();
     var inputtedDescription = $("input#new-description").val();
-    var newTask = { descript: inputtedDescription, done: "false"};
+    var newTask = { descript: inputtedDescription, done: false};
     allTasks.push(newTask);
 
     $("ul#completed").empty();
     $("ul#uncompleted").empty();
 
     allTasks.forEach(function(task) {
-      if (task.done === "true") {
+      if (task.done === true) {
         $("ul#completed").append("<li>" + task.descript + "</li>");
       } else {
         $("ul#uncompleted").append("<span class='uncompleted'><li>" + task.descript + "</li></span>");
         $(".uncompleted").last().click(function() {
-          task.done = "true";
+          task.done = true;
           $("ul#completed").append($(this).children());
         });
       }
